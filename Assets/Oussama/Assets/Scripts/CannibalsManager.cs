0b1f2a673938a45d2cfb2f54584daf6a1d37ef22
OussaKab <oussamakably@gmail.com>
2021-05-11 14:13:53 -0400

    CannibalsManager created (on wip)

---
+++ Assets/Oussama/Assets/Scripts/CannibalsManager.cs
@@ -0,0 +1,31 @@
+﻿
+public class CannibalsManager : Singleton<CannibalsManager>
+{
+
+    private EnemyFollow[] canibals;
+    
+    protected override void Awake()
+    {
+        base.Awake();
+    }
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        canibals = FindObjectsOfType<EnemyFollow>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+
+    private enum CannibalsState
+    {
+        FOLLOWING, 
+        SEARCHING, 
+        ATTACKING
+    }
+}
